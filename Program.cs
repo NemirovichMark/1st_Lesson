4e78b654f126afd9caf7f79ad9eb6674091ef245
AntonXAbolin <112975210+AntonXAbolin@users.noreply.github.com>
2022-09-09 22:33:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,168 +6,7 @@ namespace _1st_Lesson
     {
         static void Main(string[] args)
         {
-            #region Integers
-            int a, b = 5;
-            int c;
-            int d = 10;
-
-            // just for info
-            short smallIntegers = Int16.MaxValue;
-            long outstretchedInteger = Int64.MinValue;
-            uint notNegativeIntegers = 5;   // not negative
-            byte FF = 255;                  // not sigative
-            sbyte ff = -127;                // signed (+-) byte
-
-
-            a = (b / 5) + d%4 - 2; // при делении целых чисел всегда округляется вниз
-            c = -123546;
-
-            // Используйте интерполированные строки ($) для вывода и будет вам счастье
-            Console.WriteLine($"Integers: a={a}, b={b}, c={c}, d={d}");
-            #endregion
-
-            #region Doubles
-            double aa, bb = 5.0;
-            double cc;
-            double dd = 12.251;
-
-            float f = 123.4f;
-            decimal m = 123.4m;
-
-            aa = (bb / 5) + dd % 4 - 2; // тут проблем с округлением не будет
-            cc = b / 5.0; // важно, чтобы 1 из чисел было дробным, тогда будет довольно точный расчет
-            
-            Console.WriteLine($"Doubles: aa={aa}, bb={bb}, cc={cc}, dd={dd}");
-            #endregion
-
-            #region Strings
-            string s = "", str = string.Empty; // 2 пустые строки
-            string name = "Vasya";
-            string famile = "P" + "u" + "p" + "kin"; // не делайте так! Тут строка пересоздается 4 раза.
-
-            Console.WriteLine(name + " " + famile + " cool guy =)");
-            #endregion
-
-            #region Chars
-            char letter = 'a';
-            char digit = '0';
-            char symbol = '+';
-
-            Console.WriteLine($"Zero is " + digit); 
-            // тут идет преобразование символа в строку, так как строка - это набор символов
-            #endregion
- 
-                
-            #region Booleans
-            bool t = true, f;
-            f = false;
-            t = 5 > 2;
-            t = (1 == 0) || ((1 > 0) && true);
-            f = !t;
-
-            Console.WriteLine($"True is {t,10}. false is {f,-10}"); 
-            #endregion
-
-            Console.WriteLine($"Zero is " + digit); 
-            // тут идет преобразование символа в строку, так как строка - это набор символов
-            #endregion
-
-            #region Casting and type conversion
-            #region Implicit Conversions
-            aa = a;
-            b = 5;
-            bb = 5;
-            #endregion
-
-            #region Explicit conversions (casts)
-            //a = aa;
-            a = (int)aa; // Cast double to int. Приведение дробного к целому.
-            bb = (double)b; // Нет смысла из подмножества приводить к множеству double > int
-            #endregion
-
-            #region Сonversions by methods
-            s = a.ToString();
-            str = aa.ToString();
-
-            a = Int32.Parse("123");
-           // bb = Double.Parse("Hey, are you ready to errors?");
-            #endregion
-
-            // Полная таблица явных и неявных преобразований, если заинтересовало (будет полезно):
-            // https://docs.microsoft.com/ru-ru/dotnet/csharp/language-reference/builtin-types/numeric-conversions
-            #endregion
-
-            #region TryCatchFinally
-            // How to avoid errors?
-            int divizionByZero = 50;
-            try
-            {
-                // this block have to be
-                a = 0; 
-                divizionByZero = divizionByZero / a;
-            }
-            catch
-            {
-                //throw new DivideByZeroException();
-            }
-            finally
-            {
-                Console.WriteLine("Would a == 0 or not, this block would work");
-                Console.WriteLine(divizionByZero);
-            }
-
-            #region TryMethods
-            if (Int32.TryParse(str, out int numberFromString))
-            {
-                divizionByZero /= numberFromString;
-            }
-            #endregion
-
-            #endregion
-
-            #region Math class
-            a = (int)Math.Pow(5, 2);
-            bb = Math.Sin(aa); // Угол, измеряемый в радианах.
-            c = Math.Abs(c);
-            #endregion
-
-            #region First example
-            // Find the roots of the equation
-
-            // 3.2x + 8.7x^2 = 17.9 
-
-            aa = 8.7;
-            bb = 3.2;
-            cc = -17.9;
-
-            double discriminant = Math.Pow(bb, 2) - 4 * aa * cc;
-            try
-            {
-                double x1 = (-bb + Math.Sqrt(discriminant)) / 2 * aa;
-                double x2 = (-bb - Math.Sqrt(discriminant)) / 2 * aa;
-
-                Console.WriteLine($"Roots of the eqution are: {x1, 5} and {x2, 5}");
-            }
-            catch
-            {
-                Console.WriteLine("There is no rational roots");
-            }
-            #endregion
-
-            #region Second example
-            // Find the value of f(x) when x is...
-            // f(x) = 25.3x + lg(4|x|+50) - exp^(x/100)
-            Console.WriteLine("Type the integer x");
-            if(Int32.TryParse(Console.ReadLine(), out int x))
-            { 
-                aa = 25.3 * x;
-                bb = Math.Log10(4 * Math.Abs(x) + 50);
-                cc = Math.Exp(x / 100);
-                var y = aa + bb - cc; // var используйте, когда справа написан тип или вам подойдет любой принимаемый тип
-                Console.WriteLine($"The f({x}) = {y}");
-            }
-            #endregion
-
+            #region Tasks:
             /* Tasks:
              * 1. Find your BMI - body mass index
              * 
@@ -180,6 +19,118 @@ namespace _1st_Lesson
              *  a0 = (№ in the group)
              *  S = 1ddmm of your birth day
              */
+            #endregion
+            #region Task 1
+            static void BMIFormula()
+            {
+                try
+                {
+                    Console.WriteLine("You've chosen Function (Finding BMI)");
+                    Console.WriteLine("Type you mass: (it should be float)");
+                    float weight = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Type your height in cm: (it should be float)");
+                    float height = float.Parse(Console.ReadLine());
+                    height = height * 0.01f;
+                    float bmi = (float)(weight / Math.Pow(height , 2));
+                    Console.WriteLine($"Your BMI is {bmi} ");
+                    Console.ReadKey();
+                }
+                catch
+                {
+                    Console.WriteLine("Oops, that's the wrong number! \n Just kidding, you've writen something wrong :( ");
+                }
+
+            }
+            #endregion
+            
+            #region Task 2
+            static void LengthOfEllipse()
+            {
+                const double a = 15;
+                const double b = 4;
+                const double grad = 170;
+                const double pi = Math.PI;
+                try
+                {
+                    Console.WriteLine("You've chosen Function (Finding length of ellipse curve)");
+                    Console.WriteLine($"a is :{a}");
+                    Console.WriteLine($"b is :{b}");
+                    Console.WriteLine($"grad is :{grad}");
+
+                    double P = 4 * ((pi * a * b + (a - b)) / (a + b));
+                    double length = P / 360 * grad;
+
+                    Console.WriteLine($"Length is {length} ");
+                    Console.ReadKey();
+                }
+                catch
+                {
+                    Console.WriteLine("Oops, that's the wrong number! \n Just kidding, you've writen something wrong :( ");
+                }
+
+            }
+            #endregion
+            
+            #region Task 3
+            static void Step()
+            {
+                const double a1 = 17;
+                const double S = 10910;
+
+                try
+                {
+                    Console.WriteLine("You've chosen Function (Finding step of Arithmetic Sequence and n-th term)");
+                    Console.WriteLine($"a1 is {a1}");
+                    Console.WriteLine($"S is {S}");
+                    Console.WriteLine("Type n(Amount of):");
+                    double n = double.Parse(Console.ReadLine());
+                    double d = ((2 * S) / n - 2*a1) / (n - 1)  ;
+                    Console.WriteLine($"d is {d} ");
+                    double a_nth = a1 +  d*(n-1) ;
+                    Console.WriteLine($"a-nth is {a_nth} ");
+                    Console.ReadKey();
+                }
+                catch
+                {
+                    Console.WriteLine("Oops, that's the wrong number! \nJust kidding, you've writen something wrong :( ");
+                }
+
+            }
+            #endregion
+
+            
+            #region Main Part
+            
+            Console.WriteLine("Hello, dear friend! \nPlease choose Function, that you like \nThere are 3 Options: \n \t" +
+                "1) Formula of BMI \n\t2) Formula of Length of ellipse curve \n\t3) Formula of Arithmetic Sequence \n\nChoose one of them and type its number. ");
+            
+            try
+            {
+                int numOfFunc = int.Parse(Console.ReadLine());
+                switch (numOfFunc)
+                {
+                    case 1:
+                        BMIFormula();
+                        break;
+                    case 2:
+                        LengthOfEllipse();
+                        break;
+                    case 3:
+                        Step();
+                        break;
+                    default:
+                        Console.WriteLine("You are out of range. Please, try again.");
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Oops, you've writen something wrong! Please, try again.");
+            }
+        #endregion
+
+
+
         }
     }
 }
