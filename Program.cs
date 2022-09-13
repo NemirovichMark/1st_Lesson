e04fd8dbeaf21238deaadf4873d399fb361bc7aa
Mark <112826355+NemirovichMark@users.noreply.github.com>
2022-09-13 18:23:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 using System;
-
 namespace _1st_Lesson
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /* Tasks:
+             * 1. Find your BMI - body mass index
+             * 
+             * 2. Find the length of the ellipse sector curve with:
+             *  a = 100 % (№ in the group)
+             *  b = age - (a / № in the group)
+             *  grad = 10 * (№ in the group)
+             * 
+             * 3. Find the step (d) and nth term of an Arithmetic Sequence when:
+             *  a0 = (№ in the group)
+             *  S = 1ddmm of your birth day
+             */
 
             #region task№1
             // 1. Find your BMI - body mass index
@@ -93,8 +104,6 @@ namespace _1st_Lesson
 
             Console.WriteLine($"Task 3. Last element: {last_elem}\n        Step: {step}");
             #endregion
-
         }
     }
 }
-
