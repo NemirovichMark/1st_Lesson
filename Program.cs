bdb87fc7b78aa5a83a77e52bcd45615043718ca4
Temchikkk <113471230+Temchikkk@users.noreply.github.com>
2022-09-15 21:29:33 +0300

    HW upd
    I changed  2nd task and formula in 3rd task

--- Program.cs
+++ Program.cs
@@ -14,22 +14,22 @@ namespace _1st_Lesson
       #endregion 
 
       #region task 2
-      double a = 100%1;
-      double b = 17 - (a/1) ;
+      double a = 100%2;
+      double b = 17 - (a/2) ;
       double grad =( 3.14 * (10 * 1))/180 ;
       double L=3.14 * (a+b) * Math.Sin(grad) ;
       Console.WriteLine("Длина сектора " + L);
       #endregion 
 
       #region task 3
-      double a0 = 1;
+      double a0 = 2;
       double s = 10201;
       Console.WriteLine("Введите количество n-ных членов ");
       int n=Convert.ToInt32(Console.ReadLine());
       if (n>0)
       {
         double an = (2 * s) / n - a0;
-        double d = (((2 * s) / n) - 2 * a0) / (n - 1);
+        double d = (an - a0) / n;
         Console.WriteLine("Разница между членами последовательности " + d);
       }
       else
