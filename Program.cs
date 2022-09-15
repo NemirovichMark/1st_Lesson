e5e1f78041d5d5bf3b50c9856f13e13785923dc7
Temchikkk <113471230+Temchikkk@users.noreply.github.com>
2022-09-15 16:40:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,42 @@
-#region task 1
-double weigh = 68.6;
-double lenght = 1.89;
-double BMI = weigh / (lenght * lenght);
-Console.WriteLine("Мой ИМТ составояет " + BMI);
-#endregion 
+using System;
 
-#region task 2
-double a = 100%1;
-double b = 17 - (a/1) ;
-double grad =( 3.14 * (10 * 1))/180 ;
-double L=3.14 * (a+b) * Math.Sin(grad) ;
-Console.WriteLine("Длина сектора " + L);
-#endregion 
+namespace _1st_Lesson
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      #region task 1
+      double weigh = 68.6;
+      double lenght = 1.89;
+      double BMI = weigh / (lenght * lenght);
+      Console.WriteLine("Мой ИМТ составояет " + BMI);
+      #endregion 
 
-#region task 3
-double a0 = 1;
-double s = 10201;
-Console.WriteLine("Введите количество n-ных членов ");
-int n=Convert.ToInt32(Console.ReadLine());
-double an = (2 * s) / n - a0;
-double d = (((2 * s) / n) - 2 * a0) / (n - 1);
-Console.WriteLine("Разница между членами последовательности " + d);
-#endregion
+      #region task 2
+      double a = 100%1;
+      double b = 17 - (a/1) ;
+      double grad =( 3.14 * (10 * 1))/180 ;
+      double L=3.14 * (a+b) * Math.Sin(grad) ;
+      Console.WriteLine("Длина сектора " + L);
+      #endregion 
+
+      #region task 3
+      double a0 = 1;
+      double s = 10201;
+      Console.WriteLine("Введите количество n-ных членов ");
+      int n=Convert.ToInt32(Console.ReadLine());
+      if (n>0)
+      {
+        double an = (2 * s) / n - a0;
+        double d = (((2 * s) / n) - 2 * a0) / (n - 1);
+        Console.WriteLine("Разница между членами последовательности " + d);
+      }
+      else
+      {
+       Console.WriteLine("n введён не правильно "); 
+      }
+      #endregion
+    }
+  }
+}
