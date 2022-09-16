0d4a77dd17ea4e13dbdda68f06e15e18027a30fd
Anatoly Simankov <113644305+Toliyy@users.noreply.github.com>
2022-09-16 13:46:30 +0300

    Update Program.cs
    fixed bugs 1 time

--- Program.cs
+++ Program.cs
@@ -6,40 +6,49 @@ namespace _1st_Lesson
     {
         static void Main(string[] args)
         {
-            
+
 
             /* Tasks:
              * 1. Find your BMI - body mass index
             */
-            int m, h = 62, 175;
-            Console.WriteLine( m/(h*h) );
-             
-             
-             /*
-             * 2. Find the length of the ellipse sector curve with:
-             *  a = 100 % (№ in the group)
-             *  b = age - (a / № in the group)
-             *  grad = 10 * (№ in the group)
-             */
-                
-             int a = 100 % 23;
-             int b = 18 - (a / 23 );
-             int grad = 10 * 23;
-             Console.WriteLine(Math.PI * (a + b) * Math.Sin(grad));
-             
-             /* 
-             * 3. Find the step (d) and nth term of an Arithmetic Sequence when:
-             *  a0 = (№ in the group)
-             *  S = 1ddmm of your birth day
-             */
+            int m = 62;
+            double h = 1.75;
+            Console.WriteLine(m / (h * h));
+
+
+            /*
+            * 2. Find the length of the ellipse sector curve with:
+            *  a = 100 % (№ in the group)
+            *  b = age - (a / № in the group)
+            *  grad = 10 * (№ in the group)
+            */
+
+            int a = 100 % 23;
+            double b = 18 - (a / 23);
+            int grad = 10 * 23;
+            Console.WriteLine(Math.PI * (a + b) * Math.Sin(grad));
+
+            /* 
+            * 3. Find the step (d) and nth term of an Arithmetic Sequence when:
+            *  a0 = (№ in the group)
+            *  S = 1ddmm of your birth day
+            */
             int a0 = 23;
             int S = 11905;
-            int n = Console.ReadLine();
-            an = (S / n * 2) - a0;
-            d = (S / n * 2 - 2 * a0) / (n - 1);
-            Console.WriteLine(an);
-            Console.WriteLine(d);
+            int n = Int32.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+                double an = (S / n * 2) - a0;
+                double d = (S / n * 2 - 2 * a0) / (n - 1);
+                Console.WriteLine(an);
+                Console.WriteLine(d);
+            }
+            else
+            {
+                Console.WriteLine("недопустимое n");
+            }
             
+
         }
     }
 }
