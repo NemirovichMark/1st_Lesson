c75b51ae899a5b249eabc09c7c80ee35c5ecdf58
dasha <113549442+dshela@users.noreply.github.com>
2022-09-21 23:30:28 +0300

    Add files via upload
    Hello,can you help me with question 3

--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
-using System;
-
-namespace _1st_Lesson
-{
-    class Program
-    {
-        static void Main()
-        double h = 1.65;
-        int v = 44;
-        Console.Writeline(v/(h*h));
+﻿double h = 1.65;
+int v = 44;
+Console.WriteLine(v / (h * h));
 int a = 100 % 5;
-int b = 17 - (a/5);
-int grad = 10*5;
-Console.Writeline(Math.PI*(a+b)*Math.Sin(grad));
+double b = 17 - (a / 5);
+int grad = 10 * 5;
+Console.WriteLine(Math.PI * (a + b) * Math.Sin(grad));
 int a0 = 5;
 int s = 12909;
-int n = Console.Readline();
-an = (2*s)/(n-a0);
-d = (an-a0)/(n-1);
-Console.Writeline(an);
-Console.Writeline(d);
+int n1;
+string n = Console.ReadLine();
+n1 = int.Parse(n);
+if (n1> 1) 
+    double an = (s / n1 * 2) - a0;
+    double d = (s / n1 * 2 - 2 * a0) / (n1 - 1);
+    Console.WriteLine(an);
+    Console.WriteLine(d);
 
 
- 
- 
-              
-        
