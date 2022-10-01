4c5184ebaf09c63ede0e02f0c370bc404150d892
IvashevichArtem <113028846+IvashevichArtem@users.noreply.github.com>
2022-10-01 19:05:36 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,22 +6,22 @@ namespace FirstLab
     {
         static void Main(string[] args)
         {
-            #region task_0.1
-            Console.WriteLine("Task 0.1:");
+            #region task_1
+            Console.WriteLine("Task 1:");
             try
             {
                 Console.WriteLine("Insert your weight (kg):");
                 double mm = double.Parse(Console.ReadLine());
                 while (mm <= 0)
                 {
-                    Console.WriteLine("ERROR: weight cannot be negative or zero");
+                    Console.WriteLine("weight cannot be negative or zero");
                     mm = int.Parse(Console.ReadLine());
                 }
                 Console.WriteLine("Insert your height (cm):");
                 double hh = double.Parse(Console.ReadLine());
                 while (hh <= 0)
                 {
-                    Console.WriteLine("ERROR: height cannot be negative or zero");
+                    Console.WriteLine("height cannot be negative or zero");
                     hh = int.Parse(Console.ReadLine());
                 }
                 double xx1 = mm / ((hh / 100) * (hh / 100));
@@ -30,43 +30,40 @@ namespace FirstLab
 
             catch (Exception e)
             {
-                Console.WriteLine($"ERROR: Wrong input \n {e}");
+                Console.WriteLine($"Wrong input \n {e}");
             }
-
 
             #endregion
 
-
-            #region task_0.2    
-            Console.WriteLine("Task 0.2:");
-            double aa = 100 % 7;
-            double bb = 21 - (aa / 7);
-            int grad = 10 * 7;
+            #region task_2    
+            Console.WriteLine("Task 2:");
+            double aa = 100 % 9;
+            double bb = 18 - (aa / 9);
+            int grad = 10 * 9;
             double xx2 = Math.PI * (aa + bb) * grad / 360;
             Console.WriteLine($"b = {Math.Round(bb, 2)}\ngrad = {grad}\na = {aa}\n\nThe length of the ellipse sector curve: {Math.Round(xx2, 2)} \n\n\n");
             #endregion
 
-
-            #region task 0.3 
-            Console.WriteLine("Task_0.3:");
-            double aa0 = 7;
-            double ss = 12511;
+            #region task 3 
+            Console.WriteLine("Task_3:");
+            double frs_e = 9;
+            double ss = 11008;
             try
             {
                 Console.WriteLine("Insert n:");
                 double nn = double.Parse(Console.ReadLine());
                 while (nn <= 1)
                 {
-                    Console.WriteLine("ERROR: n cannot be less then 2 ");
+                    Console.WriteLine("n cannot be less then 2 ");
                     nn = int.Parse(Console.ReadLine());
                 }
-                double nth = ss * 2 / nn - aa0;
-                double dd = (nth - aa0) / (nn - 1);
+                double nth = ss * 2 / nn - frs_e;
+                double dd = (nth - frs_e) / (nn - 1);
                 Console.WriteLine($"\nStep (d) = {Math.Round(dd, 2)}\nnth term of the sequence = {Math.Round(nth, 2)}");
             }
             catch (Exception e)
             {
-                Console.WriteLine($"ERROR: Wrong input \n {e}");
+                Console.WriteLine($"Wrong input \n {e}");
             }
             #endregion
         }
